62f77360dd622b0fef6f6f738486dec88f8300cd
ashok <ashokjung@gmail.com>
2019-04-28 22:06:35 +0530

    added test for abstract factory

---
+++ src/test/java/com/ashok/DesignPatterns/abstractFactory/SicilianPizzaFactoryTest.java
@@ -0,0 +1,12 @@
+package com.ashok.DesignPatterns.abstractFactory;
+
+import org.junit.Test;
+public class SicilianPizzaFactoryTest {
+
+    @Test
+    public void testCreatePizza() throws Exception {
+        BasePizzaFactory pizzaFactory=new SicilianPizzaFactory();
+        Pizza cheesePizza=pizzaFactory.createPizza("cheese");
+        Pizza pepperoniPizza =pizzaFactory.createPizza("pepperoni");
+    }
+}
